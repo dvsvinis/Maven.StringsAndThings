d1110e718f4806ec40ade64a5b5c97a340462109
donnasvinis <dvsvinis@mac.com>
2019-05-28 20:07:04 -0400

    Completed removeString. 3 tests passed.

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -38,9 +38,25 @@ public class StringsAndThings {
      *           removeString("Hello there", "e") //  Should return "Hllo thr"
      *           removeString("Hello there", "x") // Should return "Hello there"
      */
-    public String removeString(String base, String remove){
-        return null;
+    public String removeString(String base, String remove) {
+        String result = "";
+
+        for (int i = 0; i < base.length(); i++) {
+            if (i > base.length() - remove.length()) {
+                result += base.substring(i, base.length());
+                break;
+            } else {
+                String substring = base.substring(i, i + remove.length());
+                if (!(substring.equalsIgnoreCase(remove))) {
+                    result += base.charAt(i);
+                } else {
+                    i = i + remove.length() - 1;
+                }
+            }
+
+        }return result;
     }
+
 
     /**
      * Given a string, return true if the number of appearances of "is" anywhere in the string is equal
